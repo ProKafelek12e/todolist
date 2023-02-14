c8f5954bdf26c5dc942d1fcc62f68c9583d7228f
ProKafelek12e <pro.kafelek@gmail.com>
2023-02-14 19:21:10 +0100

    deleted unused things

--- klient/app.js
+++ klient/app.js
@@ -2,13 +2,10 @@ var json = []
 async function getData(){
     const data = await fetch(`${baseurl}/gettask`)
     json = await data.json()
-    console.log("pobrane")
-    console.log(json)
     tasks()
 }
 getData()
 function tasks(){
-    console.log("task")
     document.getElementById("content").innerHTML = ""
     for(var i=0; i<=json.length-1;i++){
         const div = document.createElement("div")
@@ -17,12 +14,8 @@ function tasks(){
         const h1 = document.createElement("h1")
         h1.innerHTML = json[i].msg
 
-        //const bt = document.createElement("button")
-        //bt.innerHTML = "❌"
         div.appendChild(h1)
-        //div.appendChild(bt)
         document.getElementById("content").appendChild(div)
-        console.log("for")
     }
 }
 
