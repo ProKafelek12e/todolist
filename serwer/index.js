adffd643935503c2482fdfd83d93afbc98c6ea52
ProKafelek12e <pro.kafelek@gmail.com>
2023-09-22 22:42:58 +0200

    prepare for mongo

--- serwer/index.js
+++ serwer/index.js
@@ -3,15 +3,14 @@ const cors = require('cors')
 const app = express()
 app.use(cors())
 const port = 3000
-
-var tasks = [{msg:'test'}]
+var tasks = [{title:'Tytuł',description:'Opis',status:0}]
 app.get("/gettask",function(req,res){
     res.json(tasks)
 })
-app.get("/addtask/:msg",function(req,res){
-    const msg = req.params.msg
-
-    const tmp_msg = {'msg':msg}
+app.get("/addtask/:title/:description",function(req,res){
+    const title = req.params.title
+    const description = req.params.description
+    const tmp_msg = {'title':title,'description':description,'status':0}
     tasks.push(tmp_msg)
     res.send("dodano")
 })
